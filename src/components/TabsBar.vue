81536c5f715cc13f872611fcad94605f295a1f13
Monika Jung <m.nika.jung@gmail.com>
2024-04-12 14:13:55 +0200

    Added user combobox to TabsBar

--- src/components/TabsBar.vue
+++ src/components/TabsBar.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="tabs-bar">
-    <img class="logo-img" :src="logo" alt="Logo">
+    <img class="icon logo-icon" :src="logo" alt="Logo">
     <div v-for="tabObj in tabsSource" :key="tabObj.id">
-      <button :class="{'tablinks': true, 'chosen-tab': tabObj.id === choosenTab }" :id="tabObj.id"
+      <button :class="{ 'tablinks': true, 'chosen-tab': tabObj.id === choosenTab }" :id="tabObj.id"
         @click="openTab(tabObj.id)">{{ tabObj.name }}</button>
+    </div>
+    <img class="icon user-icon" :src="user_img" alt="User">
+    <div class="client-combobox">
+      <select class="combobox">
+        <option v-for="client in tabsSource" :key="client.id" :value="client.id">{{ client.name + ' ' + client.surname
+          }}</option>
+      </select>
     </div>
   </div>
 </template>
 
 <script>
 import logo from "../assets/books_icon.png";
+import user_img from "../assets/user_icon.png";
 
 export default ({
   name: 'tabs-bar',
@@ -19,7 +27,8 @@ export default ({
   data() {
     return {
       choosenTab: '',
-      logo: logo
+      logo: logo,
+      user_img: user_img
     }
   },
   methods: {
@@ -41,9 +50,14 @@ export default ({
   padding: 8px;
 }
 
-.logo-img {
+.icon.logo-icon {
   margin-right: 10px;
   height: 50px;
+}
+
+.icon.user-icon {
+  height: 40px;
+  padding-top: 0px;
 }
 
 .tabs-bar button {
@@ -69,6 +83,36 @@ export default ({
 
 button.tablinks.chosen-tab {
   background-color: #007bff;
+  color: #ffffff;
+  box-shadow: 0 0 5px rgba(0, 123, 255, 0.5);
+}
+
+.client-container {
+  margin-left: 20px;
+}
+
+.combobox {
+  margin-left: 10px;
+  float: right;
+  padding: 10px 6px;
+  font-size: 16px;
+  border: 1px solid #495057;
+  border-radius: 5px;
+  background-color: #343a40;
+  color: #ffffff;
+  cursor: pointer;
+}
+
+.combobox:focus {
+  border-color: #0056b3; /* Darker blue border color on focus */
+}
+
+.combobox option:checked {
+  background-color: #007bff; /* Blue background color for selected option */
+}
+
+.combobox option {
+  background-color: #343a40;
   color: #ffffff;
 }
 </style>
